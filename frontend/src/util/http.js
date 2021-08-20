14035569a76cb60adccf7e5a6e25aa4dcd9a6b24
Manuel Figueira <manuel.figueira.9@gmail.com>
2021-08-20 14:09:05 -0300

    updated api prod url

--- frontend/src/util/http.js
+++ frontend/src/util/http.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 const API_BASE_URL =
   process.env.NODE_ENV === 'production'
-    ? 'https://weighme.herokuapp.com/api'
+    ? 'https://weighme-api.herokuapp.com/api'
     : 'http://localhost:5000/api';
 
 const HTTP_HEADERS = {
